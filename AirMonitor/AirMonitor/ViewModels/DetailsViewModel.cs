62afe596ea57592db7126ceb4934a42f8c5c0f93
GitHubKubaXD <GitHubKubaXD@.github.com>
2020-07-09 13:55:04 +0200

    lab 3 rozwiązanie

--- AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
+++ AirMonitor/AirMonitor/ViewModels/DetailsViewModel.cs
@@ -5,9 +5,9 @@ using System.Runtime.CompilerServices;
 
 namespace AirMonitor.ViewModels
 {
-    public class DetailsViewModel : INotifyPropertyChanged
+    public class DetailsViewModel : BaseViewModel
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        
 
         public DetailsViewModel()
         {
@@ -83,22 +83,6 @@ namespace AirMonitor.ViewModels
         {
             get => _pressureValue;
             set => SetProperty(ref _pressureValue, value);
-        }
-
-        private void RaisePropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        private bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-
-            field = value;
-
-            RaisePropertyChanged(propertyName);
-
-            return true;
-        }
+        }      
     }
 }
